112f4711de3b8ed2ca283421ab74b7c9a33dd2e7
ye Li <yeli.88@gmail.com>
2021-04-21 11:54:30 -0400

    completed module2

---
+++ module2/airport-management/src/main/java/pluralsight/airportmanagement/domain/FlightInformation.java
@@ -0,0 +1,100 @@
+package pluralsight.airportmanagement.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDate;
+
+@Document("flights")
+public class FlightInformation {
+    @Id
+    private String id;
+
+    @Field("departure")
+    @Indexed
+    private String departureCity;
+
+    @Field("destination")
+    @Indexed
+    private String destinationCity;
+
+    private FlightType type;
+    private boolean isDelayed;
+    private int durationMin;
+    private LocalDate departureDate;
+    private Aircraft aircraft;
+
+    @Transient
+    private LocalDate createdAt;
+
+    public FlightInformation(){
+        this.createdAt = LocalDate.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    public void setDepartureCity(String departureCity) {
+        this.departureCity = departureCity;
+    }
+
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    public void setDestinationCity(String destinationCity) {
+        this.destinationCity = destinationCity;
+    }
+
+    public FlightType getType() {
+        return type;
+    }
+
+    public void setType(FlightType type) {
+        this.type = type;
+    }
+
+    public boolean isDelayed() {
+        return isDelayed;
+    }
+
+    public void setDelayed(boolean delayed) {
+        isDelayed = delayed;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public void setDurationMin(int durationMin) {
+        this.durationMin = durationMin;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(Aircraft aircraft) {
+        this.aircraft = aircraft;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+}
